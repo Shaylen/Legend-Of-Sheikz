a787bfc5d49721b040c66bd843e578ca441aedef
Sheikz <thomasmouchart@air-de-lynn.home>
2015-11-20 22:52:55 +0100

    Added stuff

---
+++ Assets/Scripts/Spells/ElectricBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricBall : MovingSpell 
+{	
+	public float fadeTime;
+	
+	void Start()
+	{
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Damageable dmg = collider.gameObject.GetComponent<Damageable>();
+		if (dmg != null)
+		{
+			dmg.doDamage(emitter, damage);
+		}
+	}
+	
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Wall") && other.bounds.Contains(transform.position))
+		{
+			rb.velocity = Vector2.zero;
+			StartCoroutine(destroyAfterSeconds(fadeTime));
+		}
+	}
+}
