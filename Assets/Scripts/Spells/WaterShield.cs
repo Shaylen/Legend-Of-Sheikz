cc1d10c3262483aabb7e74071d170255e00a9706
Sheikz <thomasmouchart@air-de-lynn.home>
2015-11-21 14:07:42 +0100

    Added watershed

---
+++ Assets/Scripts/Spells/WaterShield.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterShield : MonoBehaviour 
+{
+	public float duration;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (destroyAfterSeconds (duration));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Rigidbody2D otherRB = other.GetComponent<Rigidbody2D> ();
+		if (otherRB)
+			otherRB.velocity *= -1;
+	}
+
+	private IEnumerator destroyAfterSeconds(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		Destroy(gameObject);
+	}
+}
