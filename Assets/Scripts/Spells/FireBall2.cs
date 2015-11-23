01b255c36d02da3c8add8735daa13eb4538d144c
Sheikz <thomulus@hotmail.com>
2015-11-23 23:52:40 +0100

    Added FireBall2

---
+++ Assets/Scripts/Spells/FireBall2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBall2 : MovingSpell {
+
+    public float lifeTime;
+    private float offsetMagnitude;
+
+    // Use this for initialization
+    void Start()
+    {
+        float angle = Vector3.Angle(Vector3.down, direction);
+        if (direction.x <= 0)
+            angle *= -1;
+        transform.Rotate(new Vector3(0, 0, angle));
+        StartCoroutine(destroyAfterSeconds(lifeTime));
+        offsetMagnitude = circleCollider.offset.magnitude;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Damageable dmg = other.gameObject.GetComponent<Damageable>();
+        if (dmg != null)
+        {
+            dmg.doDamage(emitter, damage);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Vector3 center = transform.position + (Vector3)direction * offsetMagnitude;
+        if (other.gameObject.CompareTag("Wall") && other.bounds.Contains(center))
+        {
+            rb.velocity = Vector2.zero;
+            StartCoroutine(destroyAfterSeconds(0.5f));
+        }
+    }
+}
